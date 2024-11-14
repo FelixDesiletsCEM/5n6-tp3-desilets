037812dfea68551f16bf91824ea3bc586158436e
Félix Désilets <2259475@cegepmontpetit.ca>
2024-11-14 15:17:32 -0500

    Initialisation dans le projet test dans le code

--- tp3_appMobiles_desilets/lib/main.dart
+++ tp3_appMobiles_desilets/lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+//void main() {
+  //runApp(const MyApp());
+//}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
